28aa2d8a734900e446b58a39400d9eeb5e7ce396
Gweronx <117188129+Gweronx@users.noreply.github.com>
2022-11-03 19:54:26 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+from sys import argv
+def main(name):
+    f=open(name)
+    vs={}
+    rt=[]
+    ch=""
+    for line in f:
+        tokens=line.replace("\t","").replace("\n","").split()
+        if tokens[0]=="chunk":
+            ch=tokens[1]
+        elif tokens[0]==":":
+            pass
+        elif ch=="variables":
+            vs.update({tokens[0]:tokens[1].replace("//"," ").replace('"','')})
+        elif ch=="program":
+            if tokens[0]=="out":
+                print(vs[tokens[1]])
+            elif tokens[0]=="equ":
+                if vs[tokens[1]]==vs[tokens[2]]:rt.insert(0,"True")
+                else:rt.insert(0,"False")
+            elif tokens[0]=="srt":
+                vs.update({tokens[1]:rt[int(tokens[2])-1]})
+            elif tokens[0]=="add":
+                rt.insert(0,int(int(vs[tokens[1]])+int(vs[tokens[2]])))
+            elif tokens[0]=="sub":
+                rt.insert(0,int(int(vs[tokens[1]])-int(vs[tokens[2]])))
+            elif tokens[0]=="mul":
+                rt.insert(0,int(int(vs[tokens[1]])*int(vs[tokens[2]])))
+            elif tokens[0]=="div":
+                rt.insert(0,int(int(vs[tokens[1]])/int(vs[tokens[2]])))
+main(argv[1])
