875e324fe24a8be82b9dbcc25f4c9a53b84884e1
LJ <117188129+Gweronx@users.noreply.github.com>
2022-11-14 20:47:28 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 echo "NOTE: install.sh needs the Github CLI to be installed for it to work!"
 cd $HOME
-mkdir OPP
-cd OPP
+mkdir CRUMBLE
+cd CRUMBLE  
 gh repo clone Gweronx/crumble
 cd crumble
 echo 'python3 main.py $1"
